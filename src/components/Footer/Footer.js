fdfbe9f8a760a8e41ec6206b96bf465c0888a2ec
Myron <dubtwee@gmail.com>
2020-07-16 17:07:13 -0500

    adding code updates from lecture

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <footer>
+      <h3>Have fun!</h3>
+      <p>Don't forget to mind your Ps and Qs!</p>
+    </footer>
+  );
+
+}
+
+export default Footer;
